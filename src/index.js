740b5276cd590d85fa52cbf843b5cd35826b859c
Geraldi Sutanto <5717479+justomat@users.noreply.github.com>
2022-08-26 23:33:14 +0700

    add react-query

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React, { StrictMode } from 'react'
 import * as ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import './App.css'
+
+const queryClient = new QueryClient()
 
 const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container)
 
 root.render(
 	<StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</StrictMode>
 )
